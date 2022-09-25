09c65413e602ff7326a2b7884fb34c60d036300f
Britannio <britanniojarrett@gmail.com>
2022-09-25 22:11:28 +0100

    Complete Chapter 2
    1. We created a new page to show a single post (the "detail" page).
    2. We added a route to handle the id of the post and turn it into a route param, even coercing it into an integer.
    3. We created a cell to fetch and display the post.
    4. Redwood made the world a better place by making that id available to us at several key junctions in our code and even turning it into a number automatically.
    5. We turned the actual post display into a standard React component and used it in both the homepage and new detail page.

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -10,12 +10,20 @@
 import { Router, Route, Set } from '@redwoodjs/router'
 
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import PostsLayout from 'src/layouts/PostsLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
       {/* Wraps each page in the BlogLayout template */}
       <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
