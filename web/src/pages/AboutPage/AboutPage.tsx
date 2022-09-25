c597757a471f9b20484c460fc4aa196ccce30500
Britannio <britanniojarrett@gmail.com>
2022-09-25 18:41:30 +0100

    Complete Chapter 1

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,14 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      {
+        "If you find that you're spending almost all your time on theory, start turning some attention to practical things; it will improve your theories. If you find that you're spending almost all your time on practice, start turning some attention to theoretical things; it will improve your practice. ~ Donald Knutar"
+      }
+    </>
+  )
+}
+
+export default AboutPage
