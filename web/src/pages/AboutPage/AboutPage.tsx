74c44c5dcd470679b491ec8de6d50a19cc042b2d
Britannio <britanniojarrett@gmail.com>
2022-10-02 17:15:09 +0100

    fix typo

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
       {
-        "If you find that you're spending almost all your time on theory, start turning some attention to practical things; it will improve your theories. If you find that you're spending almost all your time on practice, start turning some attention to theoretical things; it will improve your practice. ~ Donald Knutar"
+        "If you find that you're spending almost all your time on theory, start turning some attention to practical things; it will improve your theories. If you find that you're spending almost all your time on practice, start turning some attention to theoretical things; it will improve your practice. ~ Donald Knuth"
       }
     </>
   )
